5c170f45ca35bc0089e7c39317c071c993ad821e
Wai Yan Kyaw <64473359+waiyankyaw961999@users.noreply.github.com>
2021-04-13 18:03:23 +0630

    Update tripadvisor.py

--- Tripadvisor/Tripadvisor/spiders/tripadvisor.py
+++ Tripadvisor/Tripadvisor/spiders/tripadvisor.py
@@ -7,21 +7,7 @@ from scrapy import Request
 class Tripadvisor(scrapy.Spider):
     name = 'tripadvisor'
 
-    start_urls = ["https://www.tripadvisor.com/Hotels-g294191-Yangon_Rangoon_Yangon_Region-Hotels.html",    # 1Yangon
-                  "https://www.tripadvisor.com/Tourism-g295408-Mandalay_Mandalay_Region-Vacations.html",    # 2Mandalay
-                  "https://www.tripadvisor.com/Hotels-g739122-Sagaing_Sagaing_Region-Hotels.html",          # 3Sagaing
-                  "https://www.tripadvisor.com/Hotels-g2036450-Naypyidaw_Mandalay_Region-Hotels.html",      # 4Naypyidaw
-                  "https://www.tripadvisor.com/Hotels-g1381170-Shan_State-Hotels.html",                     # 5Shan
-                  "https://www.tripadvisor.com/Tourism-g3576018-Tanintharyi_Region-Vacations.html",         # 6Tanintharyi
-                  "https://www.tripadvisor.com/Hotels-g3576023-Rakhine_State-Hotels.html",                  # 7Rakhine
-                  "https://www.tripadvisor.com/Hotels-g3576030-Kachin_State-Hotels.html",                   # 8Kachin
-                  "https://www.tripadvisor.com/Hotels-g3576046-Mon_State-Hotels.html",                      # 9Mon
-                  "https://www.tripadvisor.com/Hotels-g3576052-Kayin_State-Hotels.html",                    # 10Kayin
-                  "https://www.tripadvisor.com/Hotels-g4586016-Kayah_State-Hotels.html",                    # 11Kayah
-                  "https://www.tripadvisor.com/Hotels-g303655-Bago_Bago_Region-Hotels.html",                # 12Bago
-                  "https://www.tripadvisor.com/Hotels-g3576010-Ayeyarwady_Region-Hotels.html",              # 13Ayeyarwaddy
-                  "https://www.tripadvisor.com/Hotels-g739118-Magway_Magway_Region-Hotels.html"             # 14Magway
-                  ]
+    start_urls = ["https://www.tripadvisor.com/Hotels-g294190-Myanmar-Hotels.html"]
 
     # //div[@class="listing_title"]/a/@href      > for reviews for single hotel
     # //h1[@class="_1mTlpMC3"]/text()            > name of hotel
